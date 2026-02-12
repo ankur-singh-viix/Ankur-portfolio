c1321f93ef992eac4bc7127fa799365af3eeb701
Ankur Singh <ankurdakshprajapati@example.com>
2026-02-13 01:16:59 +0530

    fix error projectcard

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -83,10 +83,9 @@ export default function ProjectCard({ project, index }: ProjectCardProps) {
           style={{
             position: 'absolute',
             top: '50%', left: '50%',
-            transform: 'translate(-50%, -50%)',
             width: '80px', height: '80px',
             border: `1px solid ${project.accentColor}15`,
-            transform: 'translate(-50%, -50%) rotate(45deg)',
+            transform: 'translate(-50%, -50%) rotate(45deg) scale(1)',
           }}
         />
 
